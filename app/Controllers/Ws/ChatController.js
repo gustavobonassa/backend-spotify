0b7f147d6023d48df6d3b08aad4decbff2c92292
Gustavo Bonassa <09845549985@edu.udesc.br>
2019-07-16 16:40:27 -0300

    chat socket

--- app/Controllers/Ws/ChatController.js
+++ app/Controllers/Ws/ChatController.js
@@ -1,14 +1,20 @@
 'use strict'
 
+let messages = [];
+
 class ChatController {
     constructor({ socket, request, auth }) {
         this.socket = socket
         this.request = request
         this.auth = auth
     }
-
+    onReady(){
+        console.log(this.socket)
+        this.socket.emit('carrega', messages)
+    }
     onMessage(message) {
-        console.log(message, this.socket.topic)
+        console.log(this.socket)
+        messages.push({ ...message, username: this.auth.user.username });
         this.socket.broadcastToAll('message', { ...message, username: this.auth.user.username })
     }
 

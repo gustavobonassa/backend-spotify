56469079127038ac73cfb0a405d7814513095c30
Gumb <gustavo.bonassa1@gmail.com>
2019-07-28 15:51:07 -0300

    preparando pro heroku

--- config/yt-api.js
+++ config/yt-api.js
@@ -2,5 +2,5 @@ const Env = use('Env')
 
 module.exports = {
     type: 'video',
-    key: Env.get('YT_API')
+    key: Env.get('YT_API', process.env.YT_API)
 }
--- resources/CloudinaryService.js
+++ resources/CloudinaryService.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const Env = use('Env');
 
 cloudinary.config({
-  cloud_name: Env.get('CLOUDINARY_CLOUD_NAME'),
-  api_key: Env.get('CLOUDINARY_API_KEY'),
-  api_secret: Env.get('CLOUDINARY_API_SECRET')
+    cloud_name: Env.get('CLOUDINARY_CLOUD_NAME', process.env.CLOUDINARY_CLOUD_NAME),
+    api_key: Env.get('CLOUDINARY_API_KEY', process.env.CLOUDINARY_API_KEY),
+    api_secret: Env.get('CLOUDINARY_API_SECRET', process.env.CLOUDINARY_API_SECRET)
 });
 
 module.exports = cloudinary;

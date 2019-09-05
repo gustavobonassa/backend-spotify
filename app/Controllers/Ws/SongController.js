4e013d196e92e4a05d8eaa815b6151aa0f7961f4
GUSTAVO MIQUELLUZZI BONASSA <09845549985@edu.udesc.br>
2019-09-05 12:32:53 -0300

    teste

--- app/Controllers/Ws/SongController.js
+++ app/Controllers/Ws/SongController.js
@@ -24,7 +24,7 @@ class SongController {
         let infoSong
 
 
-        infoSong = await ytdl.getInfo(data.url, (info) => { return info });
+        infoSong = ytdl.getInfo(data.url, (info) => { return info });
 
         await video.on('progress', (chunkLength, downloaded, total) => {
             //console.log(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
